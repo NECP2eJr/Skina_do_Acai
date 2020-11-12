4eda57cbf3cf4dde075e759cacf6dad83f1c91dd
Erick Braun <erickbraun@gec.inatel.br>
2020-11-12 09:32:17 -0300

    Primeiro Commit

---
+++ SkinaAcai/src/Controle/ClsBairros.java
@@ -0,0 +1,33 @@
+package Controle;
+
+public class ClsBairros {
+
+    int cod;
+    String nome;
+    double preco;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int codigo) {
+        this.cod = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
---
+++ SkinaAcai/src/Controle/ClsConexao.java
@@ -0,0 +1,25 @@
+package Controle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class ClsConexao {
+    private static Connection con;
+
+    public static Connection getConexao() {
+        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=bdSkina;user=;password=");
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Driver não encontrado: " + e.getMessage(), "ERRO", 3);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao abrir a conexão: " + e.getMessage(), "ERRO", 1);
+        }
+
+        return con;
+
+    }
+}
